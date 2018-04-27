ba0b60e532a08e279c389988fd32f5724099986c
renren <hlsw1994@qq.com>
2018-04-27 16:36:07 +0800

    laravel-admin

---
+++ app/Models/Cat/CatModel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Cat;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CatModel extends Model
+{
+    protected $table = 'cat';
+}
---
+++ database/migrations/2018_04_27_072049_create_cat_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCatTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cat', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->unsignedInteger('parent_id')->nullable()->comment('上级分类');
+            $table->string('cat_name',100)->nullable()->comment('分类名称');
+            $table->unsignedInteger('status')->nullable()->comment('状态');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cat');
+    }
+}
