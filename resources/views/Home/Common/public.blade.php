397c89e144d24bc73808f29b6a0190f1b08b4418
renren <hlsw1994@qq.com>
2018-05-02 17:15:39 +0800

    '修改关键词描述1'

--- resources/views/Home/Common/public.blade.php
+++ resources/views/Home/Common/public.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>@yield('title')</title>
     <meta name="description" content="刘洪旭,刘洪旭的博客,个人技术博客" />
-    <meta name="keywords" content="php,laravel,thinkphp5,easyswoole,linux,swoole,mysql,js,vue" />
+    <meta name="keywords" content="php,laravel,thinkphp5,easyswoole,linux,swoole,mysql,js,vue,ajax" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <link rel="stylesheet" type="text/css" href="//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <script src="//cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
