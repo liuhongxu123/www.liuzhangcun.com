5c496555ecc4701311839bf2cd489ed9c510ef89
renren <hlsw1994@qq.com>
2018-05-02 15:11:21 +0800

    '修改首页模板的文章title'

--- resources/views/Home/Index/index.blade.php
+++ resources/views/Home/Index/index.blade.php
@@ -5,12 +5,12 @@
     <div style="padding:0% 8%; margin:2%;">
         @foreach($data as $v)
         <div class="panel panel-default">
-            <div class="panel-body"><h2><a href="/article?id={{$v['id']}}" target="_blank">windows定时执行PHP文件</a></h2></div>
+            <div class="panel-body"><h2><a href="/article?id={{$v['id']}}" target="_blank">{{$v['title']}}</a></h2></div>
             <table class="table" style="border:0px;width:50%;">
                 <tr>
                     <td style="padding:8px 8px 8px 15px;">作者：{{$v['author']}}</td>
                     <td>时间：{{$v['created_at']}}</td>
-                    <td style="border-top: 0px solid #ddd;">分类：{{$v['title']}}</td>
+                    <td style="border-top: 0px solid #ddd;">分类：{{$v['cat_name']}}</td>
                 </tr>
             </table>
             <div class="panel-body" style="border-top: 0px solid #ddd;">
